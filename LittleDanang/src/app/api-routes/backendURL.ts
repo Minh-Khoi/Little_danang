26ae9a792f544a6cb850c02fe5aee1d0734fb66c
Minh-Khoi <nguyenxngocminhkhoi@gmail.com>
2022-04-26 14:10:38 +0700

    update server side code on product

--- LittleDanang/src/app/api-routes/backendURL.ts
+++ LittleDanang/src/app/api-routes/backendURL.ts
@@ -5,7 +5,7 @@ export const hostDIR = window.location.host;
 // the app structure were set "backend directory inside frontend directory"
 
 //** This is the path link to backend directory */
-export const backendDirectory = /*"http://localhost:8888/LittleDanangServerSide"; //*/  '/LittleDanangServerSide';
+export const backendDirectory = /* hostDIR +*/  '/LittleDanangServerSide';
 // export const backendDirectory = "http://localhost:8888/LittleDanangServerSide"; //;
 
 export const backendURL = backendDirectory + "/addsession.php";
